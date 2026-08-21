b5b6668482244a50fef1a3ca5c1ad9b87dc17162
m04x92 <thomas.kautz@fh-joanneum.at>
2026-08-21 10:50:16 +0200

    t

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union_joanneum';
-$plugin->version = 2025041408.65;
+$plugin->version = 2025041408.66;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = ['theme_boost_union' => 2024100793]; //2024100793 ... 2025041403
+$plugin->dependencies = ['theme_boost_union' => 2024100796]; //2024100793 ... 2025041403
